12c4b02274be44d68e25d2fb16beb59ad878c161
YamaTaro38 <yamadotaro38@gmail.com>
2026-06-16 10:44:58 +0700

    LAPORAN MOD 6 + SC

---
+++ 2311102227_Muhammad Rafiful Hana/guided/javascript/function.js
@@ -0,0 +1,13 @@
+function tambah(a, b){ return a + b
+};
+
+var tambah = function (a, b){ return a + b
+};
+
+console.log(tambah(3, 5));
+
+var simpan = tambah(3, 5);
+console.log(tambah(simpan, 2));
+
+console.log(tambah(tambah(3, 5), 2)); //mengembalikan 10
+console.log(tambah(tambah(2, 3), 4)); //mengembalikan 9
